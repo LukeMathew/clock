1b389c9498fd87db279de1f975363d92a01b3be2
Astrocat2374 <22ignacio.luke@waipahuh.k12.hi.us>
2020-01-10 10:30:54 -1000

    first commit

--- clock.js
+++ clock.js
@@ -1,1 +1,14 @@
-console.log ("wewfwfgbvsdvsvsvfwfwf")
+$(document).ready( function() {
+  function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    // Div for Html
+    var clockDiv = document.getElementById('clock');
+    //strings for later
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+  }
+  //runs to clock
+  displayTime();
+});
